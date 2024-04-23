1b1d8ca6aa07a553a69888fb7487ae11f97ba4e0
Huw Jones <huw@pexip.com>
2024-04-23 13:48:42 +0100

    New upstream version 0.5.0

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -3,7 +3,10 @@ import csv
 from pathlib import Path
 
 from pylint.testutils import LintModuleTest
-from pylint.testutils.functional_test_file import FunctionalTestFile
+try:
+    from pylint.testutils.functional import FunctionalTestFile
+except ImportError:
+    from pylint.testutils.functional_test_file import FunctionalTestFile
 
 import pylint_twisted
 
@@ -21,6 +24,13 @@ csv.register_dialect("test", _TestDialect)
 
 def test_reactor_inference() -> None:
     test_file = FunctionalTestFile(TEST_DIR, "reactor.py")
+    lint_test = LintModuleTest(test_file)
+    lint_test.setUp()
+    lint_test.runTest()
+
+
+def test_deferred_inference() -> None:
+    test_file = FunctionalTestFile(TEST_DIR, "deferred.py")
     lint_test = LintModuleTest(test_file)
     lint_test.setUp()
     lint_test.runTest()
